940bd3d69e896d342ef32678936f962c9085c053
Jenine Marquez <jeninemarquez@Jenines-MacBook-Air.local>
2020-03-01 19:43:24 -0500

    Add JS to make all tabs appear on hover

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // It is always helpful to use comments in your code!
+var navLinks = document.getElementsByClassName('main-menu-labels');
+var dropdowns = document.getElementsByClassName('sub-menu');
+
+function toggleDropdown() {
+    for (i = 0; i < navLinks.length; i++) {
+    dropdowns[i].classList.toggle('show');
+    }
+}
+
+for (i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('mouseenter', toggleDropdown);
+    navLinks[i].addEventListener('mouseleave', toggleDropdown);
+}
